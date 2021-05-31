298ee77f86d08658b0e3fa7b87042cfa77bd6662
AlonsoGuevara15 <alonso@itic.com.pe>
2021-05-31 17:20:40 -0500

    se cambia funcion getToken 1. autenticar 2. iniciar llamada 3. poner temporizador

--- src/components/PreJoinScreens/DeviceSelectionScreen/DeviceSelectionScreen.tsx
+++ src/components/PreJoinScreens/DeviceSelectionScreen/DeviceSelectionScreen.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { makeStyles, Typography, Grid, Button, Theme, Hidden } from '@material-ui/core';
+import {makeStyles, Typography, Grid, Button, Theme, Hidden} from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import LocalVideoPreview from './LocalVideoPreview/LocalVideoPreview';
 import SettingsMenu from './SettingsMenu/SettingsMenu';
-import { Steps } from '../PreJoinScreens';
+import {Steps} from '../PreJoinScreens';
 import ToggleAudioButton from '../../Buttons/ToggleAudioButton/ToggleAudioButton';
 import ToggleVideoButton from '../../Buttons/ToggleVideoButton/ToggleVideoButton';
-import { useAppState } from '../../../state';
+import {useAppState} from '../../../state';
 import useChatContext from '../../../hooks/useChatContext/useChatContext';
 import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
+import {getErrorMessage} from "../../../state/usePasscodeAuth/usePasscodeAuth";
+
 
 const useStyles = makeStyles((theme: Theme) => ({
   gutterBottom: {
@@ -31,13 +33,14 @@ const useStyles = makeStyles((theme: Theme) => ({
   joinButtons: {
     display: 'flex',
     justifyContent: 'space-between',
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column-reverse',
-      width: '100%',
-      '& button': {
-        margin: '0.5em 0',
-      },
+    flexDirection: 'column-reverse',
+    width: '100%',
+    '& button': {
+      margin: '0.5em 0',
     },
+    /*[theme.breakpoints.down('sm')]: {
+
+    },*/
   },
   mobileButtonBar: {
     [theme.breakpoints.down('sm')]: {
@@ -55,31 +58,90 @@ const useStyles = makeStyles((theme: Theme) => ({
 interface DeviceSelectionScreenProps {
   name: string;
   roomName: string;
+  setName: (name: string) => void;
+  setRoomName: (roomName: string) => void;
+  usrTkn: string;
   setStep: (step: Steps) => void;
 }
 
-export default function DeviceSelectionScreen({ name, roomName, setStep }: DeviceSelectionScreenProps) {
+export default function DeviceSelectionScreen({
+                                                name,
+                                                roomName,
+                                                setName,
+                                                setRoomName,
+                                                usrTkn
+                                              }: DeviceSelectionScreenProps) {
   const classes = useStyles();
-  const { getToken, isFetching } = useAppState();
-  const { connect: chatConnect } = useChatContext();
-  const { connect: videoConnect, isAcquiringLocalTracks, isConnecting } = useVideoContext();
+  const {getToken, isFetching} = useAppState();
+  const {connect: chatConnect} = useChatContext();
+  const {connect: videoConnect, isAcquiringLocalTracks, isConnecting, onError} = useVideoContext();
   const disableButtons = isFetching || isAcquiringLocalTracks || isConnecting;
 
   const handleJoin = () => {
-    getToken(name, roomName).then(token => {
-      videoConnect(token);
-      process.env.REACT_APP_DISABLE_TWILIO_CONVERSATIONS !== 'true' && chatConnect(token);
+    getToken(name, roomName, usrTkn).then(async res => {
+      const json = await res.json();
+      if (res.ok) {
+        let token = json.token
+        setName(json.name)
+        setRoomName(json.room_name)
+        let endpoint = process.env.REACT_APP_DISCONNECT_PATH || ''
+
+        if (json.client === 'paciente') {
+          var current_sec = json.seconds
+
+          window.setTimeout(function () {
+            var formData = new FormData()
+            formData.append('room_sid', json.room_sid);
+            navigator.sendBeacon(endpoint, formData)
+          }, json.seconds * 1000);
+
+          var interval = window.setInterval(function () {
+            function padZero(n: number) {
+              if (n < 10) {
+                return '0'.concat(n.toString());
+              } else {
+                return n.toString();
+              }
+            }
+
+            function setTimer(seconds: number, timer: Element) {
+              (timer as HTMLElement).dataset.seconds = seconds.toString();
+              var h = Math.floor(seconds / 3600);
+              var m = Math.floor(seconds % 3600 / 60);
+              var s = seconds % 60;
+              (timer as HTMLElement).innerHTML = '('.concat(padZero(h), ':', padZero(m), ':', padZero(s), ')');
+            }
+            // console.log(current_sec)
+            if (current_sec === 0) {
+              clearInterval(interval);
+            }
+            var timers = document.querySelectorAll('.timer');
+            timers.forEach(function (timer) {
+              setTimer(current_sec, timer)
+            });
+            current_sec = current_sec - 1;
+          }, 1000);
+
+        }
+
+        videoConnect(token);
+        process.env.REACT_APP_DISABLE_TWILIO_CONVERSATIONS !== 'true' && chatConnect(token);
+
+      } else {
+        const errorMessage = getErrorMessage(json.error?.message || res.statusText);
+        onError(new Error(errorMessage));
+      }
     });
   };
 
   if (isFetching || isConnecting) {
     return (
-      <Grid container justify="center" alignItems="center" direction="column" style={{ height: '100%' }}>
+      <Grid container justify="center" alignItems="center" direction="column" style={{height: '100%'}}>
         <div>
-          <CircularProgress variant="indeterminate" />
+          <CircularProgress variant="indeterminate"/>
         </div>
         <div>
-          <Typography variant="body2" style={{ fontWeight: 'bold', fontSize: '16px' }}>
+          <Typography variant="body2" style={{fontWeight: 'bold', fontSize: '16px'}}>
             Conectando a la sala
           </Typography>
         </div>
@@ -90,34 +152,34 @@ export default function DeviceSelectionScreen({ name, roomName, setStep }: Devic
   return (
     <>
       <Typography variant="h5" className={classes.gutterBottom}>
-        Ingresar a la sala {roomName}
+        Ingresar a la consulta
       </Typography>
 
       <Grid container justify="center">
         <Grid item md={7} sm={12} xs={12}>
           <div className={classes.localPreviewContainer}>
-            <LocalVideoPreview identity={name} />
+            <LocalVideoPreview identity={name}/>
           </div>
           <div className={classes.mobileButtonBar}>
             <Hidden mdUp>
-              <ToggleAudioButton className={classes.mobileButton} disabled={disableButtons} />
-              <ToggleVideoButton className={classes.mobileButton} disabled={disableButtons} />
+              <ToggleAudioButton className={classes.mobileButton} disabled={disableButtons}/>
+              <ToggleVideoButton className={classes.mobileButton} disabled={disableButtons}/>
             </Hidden>
-            <SettingsMenu mobileButtonClass={classes.mobileButton} />
+            <SettingsMenu mobileButtonClass={classes.mobileButton}/>
           </div>
         </Grid>
         <Grid item md={5} sm={12} xs={12}>
-          <Grid container direction="column" justify="space-between" style={{ height: '100%' }}>
+          <Grid container direction="column" justify="space-between" style={{height: '100%'}}>
             <div>
               <Hidden smDown>
-                <ToggleAudioButton className={classes.deviceButton} disabled={disableButtons} />
-                <ToggleVideoButton className={classes.deviceButton} disabled={disableButtons} />
+                <ToggleAudioButton className={classes.deviceButton} disabled={disableButtons}/>
+                <ToggleVideoButton className={classes.deviceButton} disabled={disableButtons}/>
               </Hidden>
             </div>
             <div className={classes.joinButtons}>
-              <Button variant="outlined" color="primary" onClick={() => setStep(Steps.roomNameStep)}>
+              {/*<Button variant="outlined" color="primary" onClick={() => setStep(Steps.roomNameStep)}>
                 Cancelar
-              </Button>
+              </Button>*/}
               <Button
                 variant="contained"
                 color="primary"
